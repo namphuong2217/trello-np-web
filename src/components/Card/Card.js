e8478fa10a0002d6908540c70ab6fcedcc31781b
Nguyen <nam-phuong.nguyen@adesso.de>
2022-08-15 21:45:33 +0200

    Initiate sample data

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Card.scss";
+
+function Card(props) {
+
+  const { card } = props;
+
+
+  return (
+    <li className="card-item">
+      {card.cover && <img src={card.cover} className="card-cover" alt="imagasdde"/>}
+      {card.title}
+    </li>
+  );
+}
+
+export default Card;
